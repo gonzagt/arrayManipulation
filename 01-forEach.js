e4a4f2068ca18bec2e444880590355f490987b21
Gonzalo <gonzalogermantello@gmail.com>
2021-11-24 16:23:23 -0300

    01-forEach

---
+++ 01-forEach.js
@@ -0,0 +1,14 @@
+// Creamos nuestro array básico con solo 3 elementos de tipo string:
+
+const letters = ['a', 'b', 'c'];
+
+// Con un ciclo for, esto se vería así:
+
+for (let i = 0; i < letters.length; i++) {
+    const item = letters[i];
+    console.log('for ',item)
+};
+
+// Utilizando el método forEach:
+
+letters.forEach(item => console.log('forEach', item));
